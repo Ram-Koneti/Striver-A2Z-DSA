fe2409512552ff0786a8da3d9d9f11cdf50af278
Ram Koneti <108647799+Ram-Koneti@users.noreply.github.com>
2026-05-10 21:50:23 +0530

    Add solution: implement_upper_bound

---
+++ Binary_Search/BS_on_1D_Arrays/implement_upper_bound.py
@@ -0,0 +1,40 @@
+"""
+Problem:
+Implement Upper Bound
+
+Source:
+GeeksforGeeks
+
+Link:
+https://www.geeksforgeeks.org/problems/implement-upper-bound/1
+
+Approach:
+Use binary search.
+Store possible answer whenever arr[mid] > target
+and continue searching on left side.
+
+Time Complexity:
+O(log n)
+
+Space Complexity:
+O(1)
+
+Pattern:
+Binary Search
+"""
+
+def upper_bound(arr, target):
+    low = 0
+    high = len(arr) - 1
+    answer = len(arr)
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] > target:
+            answer = mid
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return answer
